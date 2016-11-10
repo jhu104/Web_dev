2728212d5bdd4ff3e73b5658acc660acd9df6099
Jay Hu <liang12hour@gmail.com>
2016-11-10 14:15:10 -0500

    allow email to be optional

--- models/User.py
+++ models/User.py
@@ -3,6 +3,6 @@ from google.appengine.ext import db
 class User(db.Model):
     username = db.StringProperty(required=True)
     password = db.StringProperty(required=True)
-    email = db.EmailProperty()
+    email = db.StringProperty()
     created = db.DateTimeProperty(auto_now_add=True)
     last_modified = db.DateTimeProperty(auto_now=True)
