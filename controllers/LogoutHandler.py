515393a1266ef0fb87b185f4c3852ca71413ca4a
Jay Hu <liang12hour@gmail.com>
2016-11-10 14:23:52 -0500

    added Logout handler

---
+++ controllers/LogoutHandler.py
@@ -0,0 +1,8 @@
+from BaseHandler import BaseHandler
+from security import hashing
+from google.appengine.ext import db
+
+class LogoutHandler(BaseHandler):
+    def get(self):
+        self.response.headers.add_header('Set-Cookie', 'name=""; Path=/')
+        self.redirect('/signup')
